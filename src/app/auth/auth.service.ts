b0a2de1f153e7b4088f9e8cdeed191f10b0f3829
Sandeep Dalal <sandeepdalal1995@gmail.com>
2020-06-04 10:45:57 +0530

    implementing auth service in auth.page.ts and app.component.ts

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -4,6 +4,19 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class AuthService {
+  private _userIsAuthenticated = false;
+
+  get userIsAuthenticated() {
+    return this._userIsAuthenticated;
+  }
 
   constructor() { }
+
+  login() {
+    this._userIsAuthenticated = true;
+  }
+
+  logout() {
+    this._userIsAuthenticated = false;
+  }
 }
