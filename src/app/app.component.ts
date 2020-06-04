ba886a3677ad82fd8624de9651072116949e37e2
Sandeep Dalal <sandeepdalal1995@gmail.com>
2020-06-04 11:05:50 +0530

    navigating to auth page after logout

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,6 +4,7 @@ import { Platform } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
 import { AuthService } from './auth/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -15,7 +16,8 @@ export class AppComponent {
     private platform: Platform,
     private splashScreen: SplashScreen,
     private statusBar: StatusBar,
-    private authService: AuthService
+    private authService: AuthService,
+    private router: Router
   ) {
     this.initializeApp();
   }
@@ -29,5 +31,6 @@ export class AppComponent {
 
   onLogout() {
     this.authService.logout();
+    this.router.navigateByUrl('/auth');
   }
 }
