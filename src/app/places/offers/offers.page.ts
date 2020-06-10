5eb95323e9c996d5cb0a0458f2dc32c82a1088a8
SandeepDalal1995 <sandeepdalal1995@gmail.com>
2020-06-10 07:53:22 +0530

    adding Swipeable List Items

--- src/app/places/offers/offers.page.ts
+++ src/app/places/offers/offers.page.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Place } from '../place.model';
 import { PlacesService } from '../places.service';
+import { IonItemSliding } from '@ionic/angular';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-offers',
@@ -10,9 +12,15 @@ import { PlacesService } from '../places.service';
 export class OffersPage implements OnInit {
   offers: Place[];
 
-  constructor(private placesService: PlacesService) { }
+  constructor(private placesService: PlacesService, private router: Router) { }
 
   ngOnInit() {
     this.offers = this.placesService.places;
+  }
+
+  onEdit(offerId: string, slidingItem: IonItemSliding) {
+    slidingItem.close(); // this will close the sliding item automatically 
+    this.router.navigate(['/','places','tabs', 'offers', 'edit', offerId]);
+    console.log("Editing Item", offerId);
   }
 }
